2acf824023f6888615b1cd32dc24b8f97fe17955
nikadm <nikise02@mail.ru>
2014-01-06 17:52:36 +0400

    2

--- SportsStore.WebUI/Infrastructure/NinjectControllerFactory.cs
+++ SportsStore.WebUI/Infrastructure/NinjectControllerFactory.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Moq;
 using Ninject;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
 
 namespace SportsStore.WebUI.Infrastructure
 {
@@ -25,7 +28,18 @@ namespace SportsStore.WebUI.Infrastructure
 
         private void AddBinding()
         {
-            
+            var mock=new Mock<IProductRepository>();
+
+            mock.Setup(m => m.Products).Returns(new List<Product>
+            {
+                new Product {Name = "Football", Price = 25},
+                new Product {Name = "Surf board", Price = 179},
+                new Product {Name = "Running shoes", Price = 95}
+               
+            }.AsQueryable());
+
+            _ninjectKernel.Bind<IProductRepository>().ToConstant(mock.Object);
+
         }
     }
 } 
