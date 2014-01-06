85b000d7c515c57b8e7ed841cc2d157268464a4a
nikadm <nikise02@mail.ru>
2014-01-06 16:48:16 +0400

    1

---
+++ SportsStore.WebUI/Infrastructure/NinjectControllerFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ninject;
+
+namespace SportsStore.WebUI.Infrastructure
+{
+    public class NinjectControllerFactory:DefaultControllerFactory
+    {
+        private readonly IKernel _ninjectKernel;
+
+        public NinjectControllerFactory()
+        {
+            _ninjectKernel=new StandardKernel();
+            AddBinding();
+        }
+
+        protected override IController GetControllerInstance(System.Web.Routing.RequestContext requestContext, Type controllerType)
+        {
+            return controllerType == null ? null : (IController) _ninjectKernel.Get(controllerType);
+
+        }
+
+        private void AddBinding()
+        {
+            
+        }
+    }
+} 
